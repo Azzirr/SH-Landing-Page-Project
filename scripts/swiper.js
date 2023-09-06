0c5ea06efaea8edf294d10f6c179777007f84fc5
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-09-06 15:22:08 +0200

    Next sections

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,5 +1,12 @@
-let swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".dottedSwiper", {
     pagination: {
         el: ".swiper-pagination",
+    },
+});
+
+const swiper2 = new Swiper(".navigationSwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
 });
